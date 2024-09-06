debae988b06d5450a61d43ccdba0f0d4284c91bd
Grand <monyx5861@gmail.com>
2024-09-06 20:30:51 +0200

    corrections to inventory moving

--- Scripts/inventory.py
+++ Scripts/inventory.py
@@ -1,12 +1,14 @@
 import requests
 import json
+import time
 
 def switch(x,y,x1,y1):
     data = {
         "a": {"x": x, "y": y}, 
         "b": {"x": x1, "y": y1}}
+    print(data)
     response = requests.post('http://192.168.100.15:2012/swap_adjacent', json=data)
-    print(response.content)
+    # print(response.content)
     message = extract_message(response.content)
     if message == "robot is already active. Please wait":
         switch(x,y,x1,y1)
@@ -31,21 +33,53 @@ def get_inventory():
     structure = json.loads(response.content)
     return structure["hold"]
 
+def is_column_sorted(col):
+    inventory = get_inventory()
+    rows = len(inventory)
+    has_empty_place = False
+    for y in range(rows-1,-1,-1):
+        if str(inventory[y][col]) == 'None':
+            has_empty_place = True
+        else:  
+            if has_empty_place:
+                return False
+    return True
+
 inventory = get_inventory()
 rows = len(inventory)
 columns = len(inventory[0])
 steps = 0
+x = 0
+# switch(10,3,10,4)
 
-while True:
-    for x in range(columns-1):
-        step = 0
-        for y in range(rows-1,-1,-1):
-            if str(inventory[y][x]) == 'None':
-                steps += 1
-            else:
-                # print(inventory[y][0])
-                if steps > 0:
+while x < columns:
+    while(not is_column_sorted(x)):
+        y = rows - 1
+        while y >= 0 and y<rows:
+                    # print(str(inventory[y][x]))
+                    if str(inventory[y][x]) == 'None':
+                        # print('None')
+                        steps += 1
+                        y -= 1
+                    else:  
+                        if steps > 0:
+                            # print("y before: " + str(y))
+                            # print("inventory before:" + str(inventory[3][10]))
+                            # print("inventory before:" + str(inventory[4][10]))
+                            for step in range(1,steps+1,1):
+                                switch(x,y+step-1,x,y+step)
+                                print('')
+                            # print("steps: " + str(steps))
+                            y = rows-1
+                            # print("y after: " + str(y))
+                            steps = 0
+                            time.sleep(1)
+                            inventory = get_inventory()
+                            
+                            # print("inventory after:" + str(inventory[3][10]))
+                            # print("inventory after:" + str(inventory[4][10]))
+                            # print(inventory[y][x])
+                        else:
+                            y -= 1
                     # print(y)
-                    for step in range(1,steps+1,1):
-                        switch(x,y+step-1,x,y+step)
-                    steps = 0
+    x = x+1
--- Scripts/main.py
+++ Scripts/main.py
@@ -1,9 +1,10 @@
 import communication
 import inventory
 
-# communication.move_to_cordinates(4296,-5278)
+communication.move_to_cordinates(4296,-5278)
 # while True:
 #     communication.buy_ressource("Shangris Station","GOLD",12)
 #     inventory.move_to_bottom()
 
-communication.sell_items("Core Station", "GOLD", 71)
+# communication.move_to_station("Aurora Station")
+# communication.sell_items("Core Station", "GOLD", 71)
